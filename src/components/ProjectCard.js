e3e360e654cf7ceb22f613cdfc4a9005181b54eb
Jason Whisnant <tasups@gmail.com>
2022-06-13 21:15:50 -0400

    ProjectCard styled

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,17 +1,33 @@
 import React from 'react';
 
 const ProjectCard = () => {
-  return(
+  return (
     <div className="projectcard-container">
       <div className="projectcard-title">
         <h4>Projects</h4>
         <button>New Project</button>
       </div>
-      <div>
-        
+      <div className="projectcard-table">
+        <table>
+          <tr>
+            <th>Project</th>
+            <th>Description</th>
+            <th>Contributors</th>
+          </tr>
+          <tr>
+            <td>Bug_Bank</td>
+            <td>An application for storing and tracking bugs</td>
+            <td>Jason Whisnant</td>
+          </tr>
+          <tr>
+            <td>Store App</td>
+            <td>Make a copy of Amazon or something</td>
+            <td>Jason Whisnant</td>
+          </tr>
+        </table>
       </div>
     </div>
-    )
+  );
 }
 
 export default ProjectCard;
