d429a1c0dab6e4187608f595967df1dbd3e401bb
Jason Whisnant <tasups@gmail.com>
2022-06-22 20:25:19 -0400

    comment list component styled, issue with first card being hidden fixed with removing justify-contents

--- src/components/projectBoard/ProjectTicketComments.js
+++ src/components/projectBoard/ProjectTicketComments.js
@@ -4,39 +4,39 @@ const ProjectTicketComments = (props) => {
   
   console.log(props)
   
-  return(
+  return (
     <section className="ticketcomments-container">
       <div className="ticketcomments-title">
         <h4>Comments</h4>
       </div>
       <form>
-      <label htmlFor="comment" hidden="hidden">Comment</label>
-      <input 
-        type="text"
-        id="comment"
-        name="comment"
-        placeholder="write comment here"
-        // value={lastName}
-        // onChange={lastNameChange}
-        required
-      />
-      <button>Comment</button>
+        <label htmlFor="comment" hidden="hidden">
+          Comment
+        </label>
+        <input
+          type="text"
+          id="comment"
+          name="comment"
+          placeholder="write comment here"
+          // value={lastName}
+          // onChange={lastNameChange}
+          required
+        />
+        <button>Comment</button>
       </form>
       <div className="ticketcomments-comment-list">
-          {
-            props.comments?.map((comment) => (
-              <CommentCard
-                key={comment.date}
-                author={comment.author}
-                comment={comment.comment}
-                date={comment.date}
-                className="ticketcomments-comment-item"
-              />
-            ))
-          }
+          {props.comments?.map((comment) => (
+            <CommentCard
+              key={comment.date}
+              author={comment.author}
+              comment={comment.comment}
+              date={comment.date}
+              className="ticketcomments-comment-item"
+            />
+          ))}
       </div>
     </section>
-    )
+  );
 }
 
 export default ProjectTicketComments;
