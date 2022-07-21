887324e7196a9f7e8fedc6c1655b935ca888af84
Jason Whisnant <tasups@gmail.com>
2022-07-20 21:00:30 -0400

    working on the ProjectCards to be in a grid, not a table so that they can be clicked as a Link

--- src/components/dashboard/ProjectCard.js
+++ src/components/dashboard/ProjectCard.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 
 const ProjectCard = (props) => {
 
@@ -7,27 +8,45 @@ const ProjectCard = (props) => {
         <h4>Projects</h4>
         <button onClick={props.handleClick}>New Project</button>
       </div>
-
-      <div className="projectcard-table">
-        <table>
-          <tbody>
-            <tr>
-              <th>Project</th>
-              <th>Description</th>
-              <th>Contributors</th>
-            </tr>
-            {props.projects?.map((project) => (
-              <tr key={project.projectTitle}>
-                <td>{project.projectTitle}</td>
-                <td>{project.description}</td>
-                <td>{project.contributors}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="projectcard-grid">
+        <div className="projectcard-headings">
+          <div>
+            <h5>Project</h5>
+          </div>
+          <div>Description</div>
+          <div>Contributors</div>
+        </div>
+        {props.projects?.map((project) => (
+          <div key={project.projectTitle} className="projectcard-cardlink">
+            {/* THE LINK BELOW SHOULD BE DYNAMIC AND LOOK SOMETHING LIKE THIS to={`/${props}/projectboard`} */}
+            <Link to="/projectboard">
+              <div className="projectcard-link ${project.}">{project.projectTitle}</div>
+            </Link>
+            <div className="projectcard-link">{project.description}</div>
+            <div className="projectcard-link">{project.contributors}</div>
+          </div>
+        ))}
       </div>
     </section>
   );
 }
 
 export default ProjectCard;
+
+/**
+ * <table>
+          <tbody>
+            <tr>
+              <th>Project</th>
+              <th>Description</th>
+              <th>Contributors</th>
+            </tr>
+ * <tr key={project.projectTitle}>
+                <td>{project.projectTitle}</td>
+                <td>{project.description}</td>
+                <td>{project.contributors}</td>
+              </tr>
+              </tbody>
+        </table>
+ * 
+ */
