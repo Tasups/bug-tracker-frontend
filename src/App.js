feff9cb7047fd0a75ed3d77784f8a71cc820d533
Jason Whisnant <tasups@gmail.com>
2022-08-18 14:46:40 +0000

    moving state to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Dashboard from './components/dashboard/Dashboard'
@@ -11,8 +12,138 @@ import ticketsData from './data/ticketsData'
 
 import './App.css'
 
+const { v4: uuidv4 } = require('uuid');
 
+  
 function App() {
+  
+  //******************* FROM DASHBOARD
+  
+  //const [projects, setProjects] = useState(issues);
+  const [newProjectTitle, setNewProjectTitle] = useState("");
+  //const [newDescription, setNewDescription] = useState("");
+  const [newContributors, setNewContributors] = useState("");
+  
+  const toggleAddProjectModal = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const projectChange = (e) => {
+    setNewProjectTitle(e.target.value);
+  };
+
+  const descriptionChange = (e) => {
+    setNewDescription(e.target.value);
+  };
+
+  const contributorsChange = (e) => {
+    setNewContributors(e.target.value);
+  };
+
+  const addNewProject = (e) => {
+    e.preventDefault();
+    const newProject = {
+      key: newProjectTitle,
+      projectTitle: newProjectTitle,
+      description: newDescription,
+      contributors: newContributors,
+    };
+    setProjects([...projects, newProject]);
+    setNewProjectTitle("");
+    setNewDescription("");
+    setNewContributors("");
+    toggleAddProjectModal();
+  };
+
+  const handleCancel = () => {
+    setNewProjectTitle("");
+    setNewDescription("");
+    setNewContributors("");
+    toggleAddProjectModal();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addNewProject(e);
+  };
+
+
+  //******************* FROM PROJECTBOARD
+    // eslint-disable-next-line
+  
+  const [projects, setProjects] = useState(projectsData)
+  const [tickets, setTickets] = useState(ticketsData)
+  const [ticketForDescription, setTicketForDescription] = useState(tickets[0])
+  const [comments, setComments] = useState(ticketsData[0].comments)
+  const [newComment, setNewComment] = useState("")
+  const [newTitle, setNewTitle] = useState("")
+  const [newDescription, setNewDescription] = useState("")
+  const [newAuthor, setNewAuthor] = useState("")
+  const [newStatus, setNewStatus] = useState("")
+  const [newPriority, setNewPriority] = useState("")
+  const [newType, setNewType] = useState("")
+  const [newETA, setNewETA] = useState("")
+  const [parentID, setParentID] = useState("")
+  
+  const addNewTicket = (e) => {
+    e.preventDefault()
+    const newTicket = {
+      title: newTitle,
+      description: newDescription,
+      author: newAuthor,
+      status: newStatus,
+      priority: newPriority,
+      type: newType,
+      eta: newETA,
+      id: uuidv4(),
+      comments: 
+      [
+        {
+          comment: "Please make comments to describe the ticket",
+          id: uuidv4(),
+          author: "Jason Whisnant",
+          date: dateConversion()
+        }
+      ]
+    }
+    setTickets([...tickets, newTicket])
+    setNewTitle("")
+    setNewDescription("")
+    setNewAuthor("")
+    setNewStatus("")
+    setNewPriority("")
+    setNewType("")
+    setNewETA("")
+    toggleAddTicketModal()
+  }
+  
+  const addNewComment = (e) => {
+    e.preventDefault()
+    const newTicketComment = {
+      comment: newComment,
+      author: "Jason Whisnant", // the author would be the person who is logged in
+      date: dateConversion(),
+      id: uuidv4(),
+    }
+    setComments([...comments, newTicketComment])
+    setNewComment("");
+    // eslint-disable-next-line
+    const ticketToUpdate = tickets.filter((ticket) => ticket.id === parentID);
+    // eslint-disable-next-line
+    const updatedTicket = ticketToUpdate[0].comments.push(newTicketComment);
+    //setTickets([...tickets, updatedTicket]);
+  }
+
+  const handleTicketClick = (e, id) => {
+    e.preventDefault()
+    const newTicket = tickets.filter(ticket => ticket.id === id)
+    let newTicketForDescription = newTicket[0]
+    setTicketForDescription(newTicketForDescription)
+    let newComment = tickets.filter(ticket => ticket.id === id)
+    let newComments = newComment[0]
+    setComments(newComments.comments)
+    setParentID(id)
+  }
 
   return (
     <Routes>
